b66ece9fb320c2090eff1d27e9cafb8ea3a1c699
d5vis <davisjbanks@gmail.com>
2025-03-15 15:23:14 -0700

    add clerk

--- lmuhacks/app/link/page.tsx
+++ lmuhacks/app/link/page.tsx
@@ -2,51 +2,60 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { IdentityTab } from "./components/IdentityTab";
 import { SearchTab } from "./components/SearchTab";
 import { ChatTab } from "./components/ChatTab";
+import { ClerkLoaded, UserButton } from "@clerk/nextjs";
 
 const LinkPage = () => {
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] min-h-screen font-[family-name:var(--font-outfit)]">
-      <div className="w-full p-4 flex items-center justify-center bg-gray-50 border-b border-gray-300">
-        <h1 className="text-2xl inline-block">
-          Link @{" "}
-          <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-lmu-crimson to-lmu-blue">
-            LMU
-          </span>
-        </h1>
+    <ClerkLoaded>
+      <div className="grid grid-rows-[auto_1fr_auto] min-h-screen font-[family-name:var(--font-outfit)]">
+        <div className="w-full p-4 flex items-center justify-center bg-gray-50 border-b border-gray-300">
+          <h1 className="text-2xl inline-block">
+            🔗{" "}
+            <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-lmu-crimson to-lmu-blue">
+              LMU
+            </span>
+          </h1>
+          <div className="absolute right-[18px] top-[18px]">
+            <UserButton />
+          </div>
+        </div>
+        <Tabs
+          defaultValue="identity"
+          className="w-full grid grid-rows-[auto_1fr] bg-gray-50"
+        >
+          <TabsList className="w-full h-full py-4 px-8 grid grid-cols-3 gap-4 bg-gray-50">
+            <TabsTrigger
+              value="identity"
+              className="flex-1 rounded-full p-2 text-lg"
+            >
+              Identity
+            </TabsTrigger>
+            <TabsTrigger
+              value="search"
+              className="flex-1 rounded-full p-2 text-lg"
+            >
+              Search
+            </TabsTrigger>
+            <TabsTrigger
+              value="chat"
+              className="flex-1 rounded-full p-2 text-lg"
+            >
+              Chat
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="identity" className="h-full">
+            <IdentityTab />
+          </TabsContent>
+          <TabsContent value="search" className="h-full">
+            <SearchTab />
+          </TabsContent>
+          <TabsContent value="chat" className="h-full">
+            <ChatTab />
+          </TabsContent>
+        </Tabs>
+        <footer className="w-full p-4 bg-white">footer</footer>
       </div>
-      <Tabs
-        defaultValue="identity"
-        className="w-full grid grid-rows-[auto_1fr] bg-gray-50"
-      >
-        <TabsList className="w-full h-full py-4 px-8 grid grid-cols-3 gap-4 bg-gray-50">
-          <TabsTrigger
-            value="identity"
-            className="flex-1 rounded-full p-2 text-lg"
-          >
-            Identity
-          </TabsTrigger>
-          <TabsTrigger
-            value="search"
-            className="flex-1 rounded-full p-2 text-lg"
-          >
-            Search
-          </TabsTrigger>
-          <TabsTrigger value="chat" className="flex-1 rounded-full p-2 text-lg">
-            Chat
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="identity" className="h-full">
-          <IdentityTab />
-        </TabsContent>
-        <TabsContent value="search" className="h-full">
-          <SearchTab />
-        </TabsContent>
-        <TabsContent value="chat" className="h-full">
-          <ChatTab />
-        </TabsContent>
-      </Tabs>
-      <footer className="w-full p-4 bg-white">footer</footer>
-    </div>
+    </ClerkLoaded>
   );
 };
 
--- lmuhacks/app/page.tsx
+++ lmuhacks/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Home() {
@@ -7,13 +8,18 @@ export default function Home() {
       <nav className="flex justify-between items-end w-full px-10 py-4">
         <Link href="/">
           <h1 className="text-2xl inline-block">
-            Link @{" "}
+            🔗{" "}
             <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-lmu-crimson to-lmu-blue">
               LMU
             </span>
           </h1>
         </Link>
-        <Link href="/link">Get Started</Link>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
       </nav>
       <main className="relative flex flex-col gap-2 items-center justify-center w-full h-full px-8 pb-4">
         <div className="flex flex-col gap-2 items-center justify-center bg-gray-50 rounded-3xl w-full h-full p-8">
@@ -27,11 +33,20 @@ export default function Home() {
             </span>{" "}
             🦁
           </div>
-          <Link href="/link">
-            <Button variant="outline" className="rounded-full mt-2">
-              Get Started
-            </Button>
-          </Link>
+          <SignedIn>
+            <Link href="/link">
+              <Button variant="outline" className="rounded-full mt-2">
+                Start linking
+              </Button>
+            </Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>
+              <Button variant="outline" className="rounded-full mt-2">
+                Sign in
+              </Button>
+            </SignInButton>
+          </SignedOut>
         </div>
       </main>
       <footer className="text-sm text-center text-gray-500 pb-4">
